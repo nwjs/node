8e95e46a97d98d54cc4d6ab0721edb06c68cda8e
Roger Wang <roger.wang@intel.com>
2016-06-08 17:40:33 +0800

    fixup! chromium-integration

--- src/node_webkit.h
+++ src/node_webkit.h
@@ -1,56 +1,6 @@
 #ifndef _NW_NODE_INTERFACE_H
 #define _NW_NODE_INTERFACE_H
 
-#if 0
-
-#include "v8.h"
-#include "uv.h"
-
-#ifdef _WIN32
-# ifndef BUILDING_NODE_EXTENSION
-#   define NODE_EXTERN __declspec(dllexport)
-# else
-#   define NODE_EXTERN __declspec(dllimport)
-# endif
-#else
-# define NODE_EXTERN __attribute__((visibility("default")))
-#endif
-
-#ifdef BUILDING_NODE_EXTENSION
-# undef BUILDING_V8_SHARED
-# undef BUILDING_UV_SHARED
-# define USING_V8_SHARED 1
-# define USING_UV_SHARED 1
-#endif
-#ifdef _WIN32
-# ifdef BUILDING_NW_NODE
-#   define NW_EXTERN __declspec(dllexport)
-# else
-#   define NW_EXTERN __declspec(dllimport)
-# endif
-#else
-# define NW_EXTERN __attribute__((visibility("default")))
-#endif
-
-namespace node {
-extern NODE_EXTERN v8::Persistent<v8::Context> g_context;
-extern NODE_EXTERN v8::Persistent<v8::Context> g_dom_context;
-
-// Forward declaration
-class Environment;
-extern NODE_EXTERN Environment* g_env;
-
-extern NW_EXTERN void OnMessage(v8::Handle<v8::Message> message, v8::Handle<v8::Value> error);
-extern NW_EXTERN v8::Handle<v8::Value> CallNWTickCallback(Environment* env, const v8::Handle<v8::Value> ret);
-
-extern NW_EXTERN void Shutdown();
-
-extern NODE_EXTERN int (*g_nw_uv_run)(uv_loop_t* loop, uv_run_mode mode);
-
-}
-
-#endif
-
 #include <vector>
 
 typedef struct _msg_pump_context_t {
@@ -93,4 +43,5 @@ typedef void (*VoidPtr3Fn)(void*, void*, void*);
 typedef void (*VoidPtr4Fn)(void*, void*, void*, void*);
 typedef void (*VoidVoidFn)();
 typedef int (*IntVoidFn)();
+typedef bool (*BoolPtrFn)(void*);
 #endif

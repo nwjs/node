d7a34234545975ccff9b7f925b34de2c4376f123
Roger Wang <roger@nwjs.io>
2018-05-09 13:57:48 +0800

    nwjs master

--- src/node_types.cc
+++ src/node_types.cc
@@ -34,7 +34,6 @@ namespace {
   V(SharedArrayBuffer)                                                        \
   V(Proxy)                                                                    \
   V(WebAssemblyCompiledModule)                                                \
-  V(ModuleNamespaceObject)                                                    \
 
 
 #define V(type) \
